0564f64fbc4a12b590c09561ca22cf430a6126df
Immortal-codes <rajanraj8979@gmail.com>
2022-02-10 01:02:54 +0530

    first commit

---
+++ Q102.js
@@ -0,0 +1,16 @@
+//Write a JavaScript program to find the number of inversions of a given array of integers
+
+function number_of_InversionsNaive(arr) {
+    var ctr = 0;
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = i + 1; j < arr.length; j++) {
+            if (arr[i] > arr[j]) 
+              ctr++;
+        }
+    }
+    return ctr;
+}
+
+console.log(number_of_InversionsNaive([0, 3, 2, 5, 9]));   
+console.log(number_of_InversionsNaive([1, 5, 4, 3]));   
+console.log(number_of_InversionsNaive([10, 30, 20, -10]));  
